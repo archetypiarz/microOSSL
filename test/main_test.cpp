f04e8a5ac28d96c3f2e576b7fe82abc40510c0ef
archetypiarz <archetypiarz@gmail.com>
2026-05-21 22:26:23 +0200

    Change first unit test to some straightforward OpenSSL usage

--- test/main_test.cpp
+++ test/main_test.cpp
@@ -1,10 +1,18 @@
 #include <gtest/gtest.h>
+#include <openssl/rsa.h>
+#include <openssl/evp.h>
+#include <openssl/err.h>
 #include <iostream>
 
 class MainTest : public ::testing::Test {
 };
 
-TEST_F(MainTest, HelloWorldTest) {
-  std::cout << "Hello world" << std::endl;
-  EXPECT_TRUE(true);
+TEST_F(MainTest, publicRsaKeyGeneratedProperly) {
+  EVP_PKEY *pkey = EVP_RSA_gen(4096);
+  if (pkey == NULL){
+    ERR_print_errors_fp(stderr);
+    std::cout << "Key generation failed.\n";
+  }
+
+  EXPECT_EQ(4096, EVP_PKEY_get_bits(pkey));
 }
